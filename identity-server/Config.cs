69cce3cac00361c5d251b10d431b3af2bdce5346
rosered <krihsana5@gmail.com>
2021-12-04 17:09:18 +0700

    initial project

---
+++ identity-server/Config.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+
+namespace identity_server
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // Machine to Machine
+                new Client{
+                    ClientId = "defaultClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "defaultApi" }
+                },
+                // OpenId for authen user, and Oauth2 authen protect resource
+                new Client{
+                    ClientId = "default2Client",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RequirePkce = false,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>(){
+                        "https://localhost:5003/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>(){
+                        "https://localhost:5003/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>{
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>{
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Address,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "defaultApi",
+                        "roles"
+                    }
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                // protech client api
+                new ApiScope("defaultApi", "Default API")
+            };
+        
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Address(),
+                new IdentityResources.Email(),
+                new IdentityResource(
+                    "roles",
+                    "Your role(s)",
+                    new List<string>() { "role" }
+                )
+            };
+        
+        public static List<TestUser> TestUsers(IConfiguration config) =>
+            new List<TestUser>
+            {
+                new TestUser{
+                    SubjectId = Guid.NewGuid().ToString(),
+                    Username = config.GetValue<string>("Rosered:User"),
+                    Password = config.GetValue<string>("Rosered:Pass"),
+                    Claims = new List<Claim>{
+                        new Claim(JwtClaimTypes.Name, "ice it"),
+                        new Claim(JwtClaimTypes.GivenName, "ice"),
+                        new Claim(JwtClaimTypes.FamilyName, "it"),
+                        new Claim(JwtClaimTypes.Email, "ice@ice.ice"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://ice.ice"),
+                        new Claim(JwtClaimTypes.Address, "my address"),
+                        new Claim(JwtClaimTypes.Role, "user")
+                    }
+                },
+                new TestUser{
+                    SubjectId = Guid.NewGuid().ToString(),
+                    Username = config.GetValue<string>("Maruko:User"),
+                    Password = config.GetValue<string>("Maruko:Pass"),
+                    Claims = new List<Claim>{
+                        new Claim(JwtClaimTypes.Name, "maruko it"),
+                        new Claim(JwtClaimTypes.GivenName, "maruko"),
+                        new Claim(JwtClaimTypes.FamilyName, "it"),
+                        new Claim(JwtClaimTypes.Email, "maruko@maruko.maruko"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://maruko.maruko"),
+                        new Claim(JwtClaimTypes.Address, "my address"),
+                        new Claim(JwtClaimTypes.Role, "user")
+                    }
+                }
+            };
+    }
+}
---
+++ identity-server/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace identity_server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string raw = Configuration.GetValue<string>("Identity:Key");
+            var key = Convert.FromBase64String(raw);
+            var cert = new X509Certificate2(key);
+            services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddTestUsers(Config.TestUsers(Configuration))
+                .AddSigningCredential(cert)
+                ;
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
